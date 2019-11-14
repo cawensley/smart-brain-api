e02eb00db4724608555f4e3ab10721ee052b99a8
Alex Wensley <cwensley@gmail.com>
2019-11-14 16:54:53 -0700

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,20 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({apiKey: '9d16a3ca28df4f0bbdc1702a1c3aa25f'});
+
+const handleAPICall = (req,res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {res.json(data)})
+        .catch(err => res.status(400).json('unable to work with API'))
+};
+
+const handleImage = (req,res,db) => {
+    const {id} = req.body;
+    db('users').where('id','=',id)
+        .increment('entries',1)
+        .returning('entries')
+        .then(entries => {res.json(entries[0]);})
+        .catch(err => res.status(400).json('unable to get entries'))
+};
+
+module.exports = {handleImage,handleAPICall};
---
+++ server.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const signin = require('./controllers/signin');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'awensley',
+        password: '',
+        database: 'smart-brain'
+}});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const PORT = process.env.PORT;
+
+app.get('/', (req,res)=> {res.send('Smart-Brain-API is working!')});
+app.post('/signin', (req,res) => {signin.handleSignIn(req,res,db,bcrypt)});
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)});
+app.get('/profile/:id',(req,res) => {profile.handleProfile(req,res,db)});
+app.put('/image', (req,res)=> {image.handleImage(req,res,db)});
+app.post('/imageurl',(req,res)=>{image.handleAPICall(req,res)});
+
+app.listen(PORT || 3000, ()=> {console.log(`App is running on port ${PORT}`)});
